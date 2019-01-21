a141bd2ab30a4977053ef22b9621892c6b51f156
vastlaan <vastlaan@mail.com>
2019-01-21 11:30:13 +0100

    once again

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ require('./routes/recepiesRoutes')(app)
 require('./routes/eventRoutes')(app)
 
 if(process.env.NODE_ENV === "production"){
+	
 	app.use(express.static("client/build"));
+
 	const path = require('path');
 	app.get('*', (req,res)=>{
 		res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
