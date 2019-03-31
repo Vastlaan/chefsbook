a856842523dcad61277dee42f939222c2b004408
vastlaan <vastlaan@mail.com>
2019-03-31 15:54:34 +0200

    maybe now, looks good, only some more nav buttons on landing page and done

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -32,10 +32,18 @@ class Dashboard extends React.Component {
 		return (
 			<div className="dashboard">
 				<div className="dashboard__landing">
-					Jakis Landing wyjebany
+					<h1>Welcome, Chef!</h1>
+					<div>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
 					<img src={Chef} alt="chef"/>
 				</div>
-				<div className="dashboard__second">
+				<div className="dashboard__third">
 					<a href={"/calendar"} className="dashboard__calendar">
 						<Calendar date={date} />
 					</a>
